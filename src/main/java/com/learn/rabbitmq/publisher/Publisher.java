0a1dd71c387f0e7f60d8f5c55c79d8d8c2c94c1a
wasif960 <wasif.ali@sltn.net>
2024-07-04 18:23:20 +0500

    -Basic publishing mechanish for json object -Creating Objects of Publisher and Consumer classes and publishing and consuming message and printing on console.

--- src/main/java/com/learn/rabbitmq/publisher/Publisher.java
+++ src/main/java/com/learn/rabbitmq/publisher/Publisher.java
@@ -2,6 +2,7 @@ package com.learn.rabbitmq.publisher;
 
 import com.learn.rabbitmq.config.QueueConnection;
 import com.rabbitmq.client.Channel;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -16,5 +17,10 @@ public class Publisher {
         Channel channel =connection.createChannel();
         channel.basicPublish("","Queue-1",null,message.getBytes());
         channel.close();
+    }
+    public void publishJsonMessage(JSONObject json) throws IOException, TimeoutException {
+        QueueConnection connection = new QueueConnection();
+        Channel channel = connection.createChannel();
+        channel.basicPublish("","Queue-1",null,json.toString().getBytes());
     }
 }
